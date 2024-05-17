7c77fec375e48eb57ce2fda20d8e8cce5e18621e
GWorld1 <thunder5613@gmail.com>
2024-05-17 18:44:08 +0100

    api for getting the list of books available in the shop

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,20 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = req.headers['authorization'];
+
+    if(!token){
+        return res.status(403).send({message:'No token provided'});
+    }
+
+    jwt.verify(token,'SecretKey',function(err,decoded){
+        if(err){
+            return res.status(500).send({message:'Failed to authenticate token'})
+        }
+
+        req.userId = decoded.id;
+        next();
+    });
 });
  
 const PORT =5000;
